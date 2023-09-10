12605ab02079145dce9458cff49915690821d484
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-10 16:24:25 -0700

    procrastination
    ima git mine

---
+++ card.js
@@ -0,0 +1,8 @@
+const details = document.querySelector('.details')
+const imgContainer = document.querySelector('.img-container')
+const getUserBtn = document.getElementById('get-user-btn')
+const url = "https://random-data-api.com/api/v2/users?response_type=json"
+
+
+
+
